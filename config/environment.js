324073867e0f77296127d5a5f9e57b8e362d51e4
Mike North <northm@yahoo-inc.com>
2015-05-21 16:31:49 -0700

    save

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'mike',
     environment: environment,
-    baseURL: '/mike',
+    baseURL: '/',
     locationType: 'hash',
     EmberENV: {
       FEATURES: {
