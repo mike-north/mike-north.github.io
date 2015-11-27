68ec97db08d0b54903e6274c1a23f1e919df7841
Mike North <michael.l.north@gmail.com>
2015-11-27 18:46:26 +0800

    sort speaking engagements by date

--- app/routes/speaking.js
+++ app/routes/speaking.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
+import _collection from 'lodash/collection';
 
 export default Ember.Route.extend({
   model() {
-    return Ember.$.get('https://gist.githubusercontent.com/mike-north/baad44e2c987e7d28994/raw').then(str => JSON.parse(str).talks);
+    return Ember.$.get('https://gist.githubusercontent.com/mike-north/baad44e2c987e7d28994/raw').then(str => JSON.parse(str).talks).then(list => {
+      return _collection.sortBy(list, function (talk) {
+        return -1 * new Date(talk.date).valueOf();
+      });
+    });
   }
 });
