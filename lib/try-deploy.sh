0174d6d093c210a245277b6622d5f5f3a0bc36de
Mike North <michael.l.north@gmail.com>
2015-12-01 15:59:53 +0800

    fix deploy script

--- lib/try-deploy.sh
+++ lib/try-deploy.sh
@@ -2,7 +2,7 @@
 echo "Attempting to generate pages"
 if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == 'main' ]]; then
   ember github-pages:commit --message "New release" --branch "main"
-  git push deploy main 2>&1 >/dev/null
+  git push deploy master 2>&1 >/dev/null
 else
   echo "Not a main build -- no deploy"
 fi
