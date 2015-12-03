28f6c01b38127ed8aa37dcc83431f85b8a27b57f
Mike North <michael.l.north@gmail.com>
2015-12-03 12:37:12 +0800

    Simplification

--- app/components/x-talk.js
+++ app/components/x-talk.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
+import Moment from 'moment';
+
+const { computed } = Ember;
 
 export default Ember.Component.extend({
-  classNames: ['x-talk']
+  classNames: ['x-talk'],
+  tagName: 'details',
+  dateString: computed('talk.date', function() {
+    return new Moment(this.get('talk.date'), 'MM-DD-YYYY').format('LL');
+  })
 });
