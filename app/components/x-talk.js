c077d09bddfbdbe5a0434e53ffd9772e213da013
Mike North <michael.l.north@gmail.com>
2016-01-18 10:51:04 +0800

    upgrades

--- app/components/x-talk.js
+++ app/components/x-talk.js
@@ -1,5 +1,21 @@
 import Ember from 'ember';
+import Moment from 'moment';
+
+const { computed } = Ember;
 
 export default Ember.Component.extend({
-  classNames: ['x-talk']
+  classNames: ['x-talk'],
+  classNameBindings: ['isUpcoming:x-talk-upcoming'],
+
+  mDate: computed('talk.date', function() {
+    return new Moment(this.get('talk.date'), 'MM-DD-YYYY');
+  }),
+
+  today: computed(function() {
+    return new Moment();
+  }),
+
+  isUpcoming: computed('mDate', 'today', function() {
+    return this.get('today').isBefore(this.get('mDate'));
+  })
 });
--- app/routes/speaking.js
+++ app/routes/speaking.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 import _collection from 'lodash/collection';
+import Moment from 'moment';
 
 export default Ember.Route.extend({
   model() {
     return Ember.$.get('https://gist.githubusercontent.com/mike-north/baad44e2c987e7d28994/raw').then(str => JSON.parse(str).talks).then(list => {
       return _collection.sortBy(list, function (talk) {
-        return -1 * new Date(talk.date).valueOf();
+        let m = new Moment(talk.date, 'MM-DD-YYYY');
+        return -1 * m.valueOf();
       });
     });
   }
